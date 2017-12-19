b6505c0473fc0d7983ccea5a40c14c33ded7bb81
awj03130 <adamweldonjohnson@gmail.com>
2017-12-19 15:59:59 -0600

    added several endpoints, imported bulma and built out registration card

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 // ----MODULE REQUIREMENTS----
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const db = require('./db/db.js');
 
 let app = express();
 
-app.set(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 // ----ENDPOINTS----
 app.get('/', (req, res) => {
-  res.render('home.hbs', {})
+  res.render('home', {
+    test: 'home'
+  })
+})
+// ------------SIGNUP-----------
+app.get('/signup', (req, res) => {
+  res.render('registration', {
+    test: 'reg'
+  })
+})
+app.post('/signup', (req, res) => {
+  db.query('', (err, results, fields) => {
+    if (err) {
+      res.send('broken forevaaaaa ')
+    }
+
+  })
+  res.render('registration', {
+    test: 'WORKING NOW AND SIGNED UP AND STUFF'
+  });
 })
 
 
 // -----LISTENING-----
 app.listen(3008, () => {
-  console.log('Now listening on port 3000');
+  console.log('Now listening on port 3008');
 })
