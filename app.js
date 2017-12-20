d7440e42178cd640d2d5778bcdec61b57864c217
awj03130 <adamweldonjohnson@gmail.com>
2017-12-20 08:51:05 -0600

    added validation for all fields in sign up page

--- app.js
+++ app.js
@@ -26,19 +26,23 @@ app.get('/', (req, res) => {
 })
 
 // -------SIGN IN-------
-app.get('/signin', (req, res) => {
+app.get('/login', (req, res) => {
   res.render('signin', {
-    heading: 'signin'
+    heading: 'Login'
   })
 })
 
-app.post('/signin', (req, res) => {
+app.post('/login', (req, res) => {
+  res.render('signin', {
+    heading: 'Logged In!'
+  })
+
+  let username = req.body.username;
+  let email = req.body.email;
   // VALIDATION GOES HERE
   // HASHING GOES HERE
   // QUERY DB FOR USERNAME
   // COMPARE HASHED PASSWORDS
-  let username = req.body.username;
-  let email = req.body.email;
 })
 // ------------SIGNUP-----------
 app.get('/signup', (req, res) => {
@@ -48,20 +52,40 @@ app.get('/signup', (req, res) => {
 })
 
 app.post('/signup', [
-  // SETS UP INPUT CHECKS FOR USERNAME LENGTH
+  // SETS UP INPUT CHECKS FOR USERNAME LENGTH, PROVIDES MSG FOR USERNAME LENGTH ERROR
   check('username').isLength({
-    min: 1
-    // PROVIDES MSG FOR USERNAME LENGTH ERROR
-  }).withMessage('Username required')
+    min: 4,
+    max: 45
+  }).withMessage('Username must be 4-45 characters.'),
+  // CHECKS EMAIL FIELD TO MAKE SURE IT'S AN EMAIL, PROVIDES MSG FOR INVALID EMAIL ADDRESS
+  check('email').isEmail().withMessage('Invalid email address.'),
+  // CHECKS EMAIL LENGTH IS BETWEEN 6 AND 100, PROVIDES ERR MSG
+  check('email').isLength({
+    min: 6,
+    max: 100
+  }).withMessage('Email address must be between 4-100 characters.'),
+  // CHECKS PASSWORD IS BETWEEN 8 AND 30 CHARACTERS, PROVIDES ERR MSG
+  check('password').isLength({
+    min: 8,
+    max: 30
+  }).withMessage('Password must be between 8-100 characters.'),
+  // CHECKS PASSWORD WITH REG EXP'S TO ENSURE LOWERCASE, UPPERCASE, NUMBER, AND SPECIAL CHAR ARE USED, PROVIDES ERR MSG
+  check("password").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i").withMessage("Password must contain one lowercase character, one uppercase character, a number, and a special character."),
+  // CHECKS PASSWORD CONFIRMATION IS EQUAL TO THE PASSWORD, PROVIDES ERR MSG
+  check('passwordConfirm').custom((value, {
+    req
+  }) => value === req.body.password).withMessage('Passwords do not match')
+
 ], (req, res) => {
   // PUTS VALIDATION ERRORS INTO ERR VARIABLE
   const err = validationResult(req)
   // RUNS IF ERRORS ARE NOT EQUAL TO ZERO
-  if (!errors.isEmpty()) {
+  if (!err.isEmpty()) {
     console.log(err.mapped());
     // RENDERS PAGE WITH ERROR MESSAGE
-    res.render('register', {
-      heading: 'Registration failed'
+    res.render('registration', {
+      heading: 'Registration failed',
+      errors: err.array()
     });
     // CODE BELOW RUNS IF NO CHECK ERRORS FOUND
   } else {
